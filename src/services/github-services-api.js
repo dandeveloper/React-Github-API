a8e945ff2e75fb3c31cfcd71599b37a2a0eb75af
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-21 19:50:12 -0300

    Creating github-services-api

---
+++ src/services/github-services-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const Authorization = 'token 5318ccc4f59bb9a72d68ec87ffecc86a533a8bb6';
+const headers = {
+  Authorization,
+  Accept: 'application/vnd.github.v3+json',
+};
+
+export function fetchUser(username) {
+  return axios.get(`https://api.github.com/users/${username}`, {
+    headers,
+  });
+}
+
+export function fetchUserRepos(username) {
+  return axios.get(`https://api.github.com/users/${username}/repos`, {
+    headers,
+  });
+}
+
+export function fetchRepo(fullName) {
+  return axios.get(`https://api.github.com/repos/${fullName}`, {
+    headers,
+  });
+}
