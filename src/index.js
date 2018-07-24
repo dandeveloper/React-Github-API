b4674164d5da51a2a5f7c1fb4725192af883efb0
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-23 21:38:56 -0300

    Adding font-awesome and normalize

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import 'normalize.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 const store = configureStore();
 
