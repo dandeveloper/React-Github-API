2253c017e77dbd25a4ff315c23f414a79b1d43f0
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-23 21:44:32 -0300

    Created repoDetails reducer

---
+++ src/reducers/repoDetails.js
@@ -0,0 +1,30 @@
+export function repoDetailsHasErrored(state = false, action) {
+    switch (action.type) {
+        case 'REPO_DETAILS_HAS_ERRORED':
+            return action.repoDetailsErrored;
+
+        default:
+            return state;
+    }
+}
+
+export function repoDetailsIsLoading(state = false, action) {
+    switch (action.type) {
+        case 'REPO_DETAILS_IS_LOADING':
+            return action.repoDetailsIsLoading;
+
+        default:
+            return state;
+    }
+}
+
+export function repoDetails(state = {}, action) {
+    console.log('state', state);
+    switch (action.type) {
+        case 'REPO_DETAILS_FETCH_DATA_SUCCESS':
+            return action.repoDetails;
+
+        default:
+            return state;
+    }
+}
