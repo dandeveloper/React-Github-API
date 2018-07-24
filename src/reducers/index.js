49c96c8624b0f0036f2af8fb872b99ee2f4420f1
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-23 21:45:43 -0300

    Adding repoDetails reducers to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { repos, reposHasErrored, reposIsLoading } from './repos';
+import { repoDetails, repoDetailsHasErrored, repoDetailsIsLoading } from './repoDetails';
 
 export default combineReducers({
     repos,
     reposHasErrored,
-    reposIsLoading
+    reposIsLoading,
+    repoDetails,
+    repoDetailsHasErrored,
+    repoDetailsIsLoading,
 });
